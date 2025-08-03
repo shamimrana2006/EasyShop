84c11f2f00080fcaec5f4749dc6a69b17fa972bb
shamimranapro85 <shamimranaprofessional@gmail.com>
2025-08-04 01:09:35 +0600

    deploy checking secure variable on vercel

--- Easyshop-backend/controller/auth-controller.js
+++ Easyshop-backend/controller/auth-controller.js
@@ -97,7 +97,7 @@ const login_controller = async (req, res) => {
           process.env.SECRETE_JWT_KEY,
           "20m"
         );
-        const isSecure = req.protocol;
+        const isSecure = req.secure;
         cookieGenerate(res, {
           cookieName: "token",
           cookieValue: token,
